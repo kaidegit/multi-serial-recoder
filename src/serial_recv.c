5f07cfc31ef65ad1c080582983ee07f9c2c175ac
kaidegit <2857693944@qq.com>
2022-07-10 21:35:04 +0800

    fix 串口丢长度问题

--- src/serial_recv.c
+++ src/serial_recv.c
@@ -43,7 +43,7 @@ uint8_t rx_len[5];
 rt_err_t uart_irq_cb(rt_device_t serial_dev, rt_size_t size) {
     for (uint8_t i = 0; i < 5; i++){
         if (serial_dev == rx_serial[i]) {
-            rt_mq_send(uart_queue_handle, &i, 1);
+            rt_mq_send(uart_queue_handle, &i, sizeof(i));
             return RT_EOK;
         }
     }
@@ -55,33 +55,37 @@ void rx_recv(void *para) {
     char send[256];
     uint8_t serial_index;
 
-    uart_queue_handle = rt_mq_create("uart_queue", 1, 20, RT_IPC_FLAG_FIFO);
+    uart_queue_handle = rt_mq_create("uart_queue", sizeof(serial_index), 255, RT_IPC_FLAG_FIFO);
 
     for (uint8_t i = 0; i < 5; i++) {
         rx_serial[i] = rt_device_find(serial_list[i]);
-        rt_device_open(rx_serial[i], RT_DEVICE_FLAG_INT_RX);
+        rt_device_open(rx_serial[i], RT_DEVICE_FLAG_RX_NON_BLOCKING);
         rt_device_set_rx_indicate(rx_serial[i], uart_irq_cb);
     }
-
+// TODO CDC包长限制64
     while (1) {
-        if (rt_mq_recv(uart_queue_handle, &serial_index, 1, RT_WAITING_FOREVER) == RT_EOK) {
+        if (rt_mq_recv(uart_queue_handle, &serial_index, sizeof(serial_index), RT_WAITING_FOREVER) == RT_EOK) {
             rt_device_t serial = rx_serial[serial_index];
             char *buffer = rx_buffer[serial_index];
             uint8_t *len = &rx_len[serial_index];
             const char *tag = serial_list[serial_index];
 
-            rt_device_read(serial, -1, &ch, 1);
+            rt_device_read(serial, 0, &ch, 1);
             if ((ch != '\n') && (*len < 240)) {
                 buffer[(*len)++] = ch;
             } else {
                 sprintf(send, "[%s]%.*s",
                         tag, *len, buffer);
                 usbd_print(send, *len - 1 + 7);
-                DB_InsertLog(send, *len - 1 + 7);
-                DB_ReadAllLogs();
+                rt_kprintf("%.*s\r\n", *len - 1 + 7, send);
+//                DB_InsertLog(send, *len - 1 + 7);
+//                DB_ReadAllLogs();
+                // TODO 队列实现存储和发送
                 *len = 0;
                 memset(buffer, 0, 256 * sizeof(char));
             }
+//            }
+
         }
     }
 }
